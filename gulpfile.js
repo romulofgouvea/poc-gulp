d0884997726c26e55e70fc590521901a78ce23bf
Romulo Fernandes Gouvea <romulo.gouvea@vetta.digital>
2021-12-21 16:49:16 -0300

    Ajustes na geração da tarefa de release

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,39 @@ const cp = require("child_process");
 
 const PATH_OUTPUT_DEFAULT = "C:/dist";
 const OUTPUT_NAME_FOLDER = "tempBuildRelease";
-const PATH_WEB_FOLDER = "C:/workspace/sgc/001.029.000/MRN-Contratos/srv/Web";
+const PATH_WEB_FOLDER = "C:/";
+const NAME_PROJECT = "Project";
 
 const ARCHIVES_NAME_REMOVE = [
   "appsettings.Development.json",
   "appsettings.json",
   "web.config",
 ];
+
+// fetch command line arguments
+const arg = ((argList) => {
+  let arg = {},
+    a,
+    opt,
+    thisOpt,
+    curOpt;
+  for (a = 0; a < argList.length; a++) {
+    thisOpt = argList[a].trim();
+    opt = thisOpt.replace(/^\-+/, "");
+
+    if (opt === thisOpt) {
+      // argument value
+      if (curOpt) arg[curOpt] = opt;
+      curOpt = null;
+    } else {
+      // argument name
+      curOpt = opt;
+      arg[curOpt] = true;
+    }
+  }
+
+  return arg;
+})(process.argv);
 
 const clean = () => {
   return del([`${PATH_OUTPUT_DEFAULT}`], {
@@ -22,7 +48,14 @@ const clean = () => {
 
 const build = () => {
   return cp.exec(
-    `cd ${PATH_WEB_FOLDER} && dotnet publish -c release -o ${PATH_OUTPUT_DEFAULT}/${OUTPUT_NAME_FOLDER}`
+    `cd ${PATH_WEB_FOLDER} && dotnet publish -c release -o ${PATH_OUTPUT_DEFAULT}/${OUTPUT_NAME_FOLDER}`,
+    (err, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      if (err) {
+        console.log(err);
+      }
+    }
   );
 };
 
@@ -33,26 +66,28 @@ const cleanUnecessaryFiles = () => {
   return del(paths, { force: true });
 };
 
-const zipFolder = () => {
+const zipFolder = async () => {
   var date = new Date();
   var dateString =
     date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
-  var nameOutputZip = dateString + "_" + date.getTime() + ".zip";
+  var nameOutputZip = `${NAME_PROJECT}_${dateString}_${date.getTime()}.zip`;
 
-  gulp
+  await gulp
     .src(`${PATH_OUTPUT_DEFAULT}/${OUTPUT_NAME_FOLDER}/**`)
     .pipe(zip(nameOutputZip))
-    .pipe(gulp.dest(`${PATH_OUTPUT_DEFAULT}`));
-
-    cp.exec(`start ${PATH_OUTPUT_DEFAULT}`)
+    .pipe(gulp.dest(`${PATH_OUTPUT_DEFAULT}`))
+    .pipe(cp.exec(`start ${PATH_OUTPUT_DEFAULT}`));
 };
 
 // exports.clean = clean;
 // exports.build = build;
 // exports.cleanFiles = cleanUnecessaryFiles;
-exports.releaseHomolog = gulp.series(
-  clean,
-  build,
-  cleanUnecessaryFiles,
-  zipFolder
-);
+exports.homolog = gulp.series(clean, build, cleanUnecessaryFiles, zipFolder);
+
+exports.release = (done) => {
+  if (arg.a == "homolog") {
+    console.log("Gerando Release Homologação");
+    exports.homolog();
+    done();
+  }
+};
